988c2cf04d3373271e8842c3c33bf9bc88036325
CT Chen <ct.chen@ischool.com.tw>
2024-06-12 16:51:16 +0800

    高雄國中評量查詢，加入60分以下組距與科目班級平均。

--- fd119cfe-03ab-7017-30b1-ad95bd89b91e/src/RankDetail.js
+++ fd119cfe-03ab-7017-30b1-ad95bd89b91e/src/RankDetail.js
@@ -17,15 +17,18 @@ const RankDetail = () => {
     const subject = appData.subject;
     const domain = appData.domain;
     const storageScore = appData.score;
-
+    
     const exam_type = appData.exam_type;
     const total_type = appData.total_type;
+    
+    // 科目加權平均
+  const [SubjectWeightedAverage, setSubjectWeightedAverage] = useState(0);
 
     // 班級學生組距資料
     const [scoreLevelData, setScoreLevel] = useState([]);
 
     //長條圖
-    const [levelList, setLevelList] = useState([{ name: "100", count: 0 }, { name: "90-99", count: 0 }, { name: "80-89", count: 0 }, { name: "70-79", count: 0 }, { name: "60-69", count: 0 }, { name: "<60", count: 0 }]);
+    const [levelList, setLevelList] = useState([{ name: "100", count: 0 }, { name: "90-99", count: 0 }, { name: "80-89", count: 0 }, { name: "70-79", count: 0 }, { name: "60-69", count: 0 },{ name: "50-59", count: 0 }, { name: "40-49", count: 0 }, { name: "30-39", count: 0 }, { name: "20-29", count: 0 }, { name: "10-19", count: 0 }, { name: "0-9", count: 0 }]);
 
     //取長條圖最大值
     const [chartMax, setChartMax] = useState(0);
@@ -92,6 +95,9 @@ const RankDetail = () => {
                 let score = Number(storageScore);
                 let position_name = '';
 
+                // 取得加權平均
+                setSubjectWeightedAverage(Number(data.weighted_average));
+
                 if (score >= 100)
                     position_name = '100';
                 if (score >= 90 && score < 100)
@@ -102,15 +108,33 @@ const RankDetail = () => {
                     position_name = '70-79';
                 if (score >= 60 && score < 70)
                     position_name = '60-69';
-                if (score < 60)
-                    position_name = '<60';
+                // if (score < 60)
+                //     position_name = '<60';
+                if (score >= 50 && score < 60)
+                    position_name = '50-59';
+                if (score >= 40 && score < 50)
+                    position_name = '40-49';
+                if (score >= 30 && score < 40)
+                    position_name = '30-39';
+                if (score >= 20 && score < 30)
+                    position_name = '20-29';
+                if (score >= 10 && score < 20)
+                    position_name = '10-19';
+                if (score >= 0 && score < 10)
+                    position_name = '0-9';               
 
                 source["100"] = Number(data.level_gte100);
                 source["90-99"] = Number(data.level_90);
                 source["80-89"] = Number(data.level_80);
                 source["70-79"] = Number(data.level_70);
                 source["60-69"] = Number(data.level_60);
-                source["<60"] = Number(data.under60);
+                // source["<60"] = Number(data.under60);
+                source["50-59"] = Number(data.level_50);
+                source["40-49"] = Number(data.level_40);
+                source["30-39"] = Number(data.level_30);
+                source["20-29"] = Number(data.level_20);
+                source["10-19"] = Number(data.level_10);
+                source["0-9"] = Number(data.level_0);
 
                 const merge = Object.getOwnPropertyNames(source).map(v => ({
                     name: v,
@@ -272,7 +296,7 @@ const RankDetail = () => {
 
 
     return (
-        <div className="App">
+        <div className="App">            
             <div className="container px-3 px-sm-4 py-5 ">
 
                 <div className="d-flex justify-content-between">
@@ -286,7 +310,8 @@ const RankDetail = () => {
                             <div className='fs-2 text-white me-1 row align-items-center justify-content-center' style={{ width: '80px', height: '80px', background: "#5B9BD5" }}>{subject === '加權平均' || subject === '算術平均' || subject === '' ? Math.round(Number(storageScore) * 100) / 100 : storageScore}</div>
 
                             <div className='fs-4 fw-bold'>{domain === "" ? "" : subject === "" ? domain : domain + "-"}{subject === '加權平均' || subject === '算術平均' ? subject + '(' + total_type + ')' : subject}</div>
-                        </div>
+                            <div className='ms-4' style={{ visibility: subject !== "" && !isNaN(SubjectWeightedAverage) && SubjectWeightedAverage !== null && SubjectWeightedAverage !== undefined && SubjectWeightedAverage !== '' ? 'visible' : 'hidden' }}>班級平均：{SubjectWeightedAverage}</div>                            
+                        </div>                        
                     </div>
 
                     <div className='col align-self-end'>
